5d1dbefa3967dfc5d6d73a2c782ad1a7649dbc7c
Yunus SANDIKCI <yunussandikci@outlook.com>
2019-04-08 14:47:14 +0300

    Project init

---
+++ src/main/java/com/yunussandikci/GithubImporter/GithubImporterApplication.java
@@ -0,0 +1,13 @@
+package com.yunussandikci.GithubImporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubImporterApplication.class, args);
+	}
+
+}
