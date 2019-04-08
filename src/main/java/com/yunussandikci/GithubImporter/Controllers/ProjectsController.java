c096bb940d57ef85d06dbe8f2c1acea2ed6233d1
Yunus SANDIKCI <yunussandikci@outlook.com>
2019-04-08 23:02:28 +0300

    Search projects via username in db added.

--- src/main/java/com/yunussandikci/GithubImporter/Controllers/ProjectsController.java
+++ src/main/java/com/yunussandikci/GithubImporter/Controllers/ProjectsController.java
@@ -1,21 +1,31 @@
 package com.yunussandikci.GithubImporter.Controllers;
 
 import com.yunussandikci.GithubImporter.Models.GithubAPI.Project;
+import com.yunussandikci.GithubImporter.Repositories.OwnerRepository;
 import com.yunussandikci.GithubImporter.Repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/projects")
 public class ProjectsController {
+
     @Autowired
     private ProjectRepository projectRepository;
+    @Autowired
+    private OwnerRepository ownerRepository;
 
     @GetMapping("/")
     public Iterable<Project> getAllImportedProject() {
         return projectRepository.findAll();
+    }
+
+    @GetMapping("/search/{keyword}")
+    public Iterable<Project> searchInImportedProject(@PathVariable("keyword") String keyword) {
+        return projectRepository.findProjectsByUsername(keyword);
     }
 
 }
--- src/main/java/com/yunussandikci/GithubImporter/Repositories/ProjectRepository.java
+++ src/main/java/com/yunussandikci/GithubImporter/Repositories/ProjectRepository.java
@@ -1,6 +1,14 @@
 package com.yunussandikci.GithubImporter.Repositories;
 
 import com.yunussandikci.GithubImporter.Models.GithubAPI.Project;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface ProjectRepository extends CrudRepository<Project, Integer> { }
+import java.util.List;
+
+public interface ProjectRepository extends CrudRepository<Project, Integer> {
+
+    @Query(value = "SELECT * FROM project p inner join owner o where p.owner_id=o.id and o.login = :username", nativeQuery = true)
+    List<Project> findProjectsByUsername(@Param("username") String username);
+}
