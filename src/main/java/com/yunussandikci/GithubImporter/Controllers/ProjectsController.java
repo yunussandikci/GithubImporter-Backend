076a3b4ce05dcb2006a46d6e9dbeb04830dce693
Yunus SANDIKCI <yunussandikci@outlook.com>
2019-04-10 10:22:13 +0300

    some todos added.

--- src/main/java/com/yunussandikci/GithubImporter/Controllers/ProjectsController.java
+++ src/main/java/com/yunussandikci/GithubImporter/Controllers/ProjectsController.java
@@ -4,10 +4,7 @@ import com.yunussandikci.GithubImporter.Models.GithubAPI.Project;
 import com.yunussandikci.GithubImporter.Repositories.OwnerRepository;
 import com.yunussandikci.GithubImporter.Repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/projects")
@@ -15,14 +12,14 @@ public class ProjectsController {
 
     @Autowired
     private ProjectRepository projectRepository;
-    @Autowired
-    private OwnerRepository ownerRepository;
 
     @GetMapping("/")
     public Iterable<Project> getAllImportedProject() {
         return projectRepository.findAll();
     }
 
+    ///TODO: Delete crossorigin on deployment phase"
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/search/{keyword}")
     public Iterable<Project> searchInImportedProject(@PathVariable("keyword") String keyword) {
         return projectRepository.findProjectsByUsername(keyword);
--- src/main/java/com/yunussandikci/GithubImporter/Repositories/ProjectRepository.java
+++ src/main/java/com/yunussandikci/GithubImporter/Repositories/ProjectRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface ProjectRepository extends CrudRepository<Project, Integer> {
 
+
+    //TODO: Check for sql injection
     @Query(value = "SELECT * FROM project p inner join owner o where p.owner_id=o.id and o.login = :username", nativeQuery = true)
     List<Project> findProjectsByUsername(@Param("username") String username);
 }
